bfdbe946b2b2235cfd5f07b0eb781823ee8cf012
Giuliano Gostinfini <giuliano.gostinfini.93@gmail.com>
2022-03-31 09:17:39 +0200

    initial commit

---
+++ src/components/calendar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { format } from 'date-fns'
+
+
+class Calendar extends React.Component {
+    state = {
+        currentMonth: new Date(),
+        selectedDate: new Date()
+    }
+
+    renderHeader(){
+        const dateFormat = "dd mm yyyy"
+        return (
+        <div className="header row flex-middle">
+            <div className="col col-start">
+                <div className="icon" onClick={this.prevMonth}>
+                    chevron_left
+                </div>
+            </div>
+            <div className="col col-center">
+                <span>
+                    {format(this.state.currentMonth, dateFormat)}
+                </span>
+            </div>
+            <div className="col col-end" onClick={this.nextMonth}>
+                <div className="icon">chevron_right</div>
+            </div>
+      </div>
+      )
+
+    }
+    renderDays(){}
+    renderCells(){}
+
+
+    onDateClick = day => {}
+
+    nextMonth = ()=>{}
+
+    prevMonth = ()=>{}
+
+
+    render(){
+        return (
+            <div className="calendar">
+                {this.renderHeader()}
+                {this.renderDays()}
+                {this.renderCells()}
+            </div>
+          );
+    }
+}
+
+
+export default Calendar;
